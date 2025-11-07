e4551d6e74772ce951c2d0448f53a07b00e8048f
Debra Mugwe <debramugwe@gmail.com>
2025-11-07 02:09:56 -0800

    1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,6 @@
+let language_1 = "C is fun";
+let language_2 = "Python is cool";
+let language_3 = "JavaScript is amazing";
+ console.log(language_1);
+ console.log(language_2);
+ console.log(language_3);
